0d3546fa2f6adf1d7998a65e43b6ab79a8805701
Chaitanya Suvarna <chaitanyasuvarna@Chaitanyas-MacBook-Air.local>
2020-04-14 15:14:29 +0530

    Completed course and finished creating a starter kit

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,3 +1,15 @@
 export default function getBaseUrl() {
-  return 'http://localhost:3001/';
+  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://evening-beach-38363.herokuapp.com/';
+}
+
+/* eslint-disable no-useless-escape */
+
+function getQueryStringParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
